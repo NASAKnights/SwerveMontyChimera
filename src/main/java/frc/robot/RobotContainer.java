d894c61a8024fb7c5f338fe3d7858b0fe4de69af
LeviWalker <levi.j.walker04@gmail.com>
2023-05-22 17:45:53 -0400

    remove wordy wpilib comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,12 +29,6 @@ import frc.robot.shooter.Shooter;
 import frc.robot.shooter.commands.SpinUpShooter;
 import frc.robot.shooter.commands.StopShooter;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
     private Joystick driver;
     private Joystick operator;
@@ -46,7 +40,6 @@ public class RobotContainer {
     private Intake intake;
 
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
 
     driver = new Joystick(Constants.kDriverPort);
@@ -68,16 +61,6 @@ public class RobotContainer {
     configureDefaultCommands();
     configureButtonBindings();
   }
-
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
 
   private void configureDefaultCommands(){
     swerve.setDefaultCommand(new Drive(driver, swerve));
@@ -123,14 +106,7 @@ public class RobotContainer {
     swerve.updateOffsets();
   }
   
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return new InstantCommand();
   }
 }
